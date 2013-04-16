d8441135a18bb8de54c1aacc8cdf12843dcc5b3c
Shinya Hiruta <s07802sh@gmail.com>
2013-04-17 01:12:27 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+# Create symbolic links
 FILES=(.vim .vimrc .gvimrc .screenrc)
 
 for file in ${FILES[@]}
 do
+  origfile=$HOME/$file
+  if [ -L ${origfile} ]; then
+    echo "Remove existing symbolic link"
+    rm -v ${origfile}
+  elif [ -f ${origfile} -o -d ${origfile} ]; then
+    echo "Backup original file"
+    mv ${origfile} ${origfile}_`date +%Y%m%d`
+  fi
+
+  echo "Create symbolic link: $HOME/$file"
   ln -s $HOME/dotfiles/$file $HOME/$file
 done
+
+# Install neobundle.vim
+if [ ! -d ~/.vim/bundle ]; then
+  mkdir -p ~/.vim/bundle
+  git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+fi
